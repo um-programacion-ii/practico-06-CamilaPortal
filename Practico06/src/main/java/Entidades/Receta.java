94eb28bfaf7e1ab969483d92aaead4528d8b7b60
CamilaPortal <c.portal@alumno.um.edu.ar>
2024-05-14 16:40:11 -0300

    Servicios y tests terminados

--- Practico06/src/main/java/Entidades/Receta.java
+++ Practico06/src/main/java/Entidades/Receta.java
@@ -10,9 +10,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 
 public class Receta {
+    private static int contadorIds = 0;
 
     private int id;
     private Paciente paciente;
     private Medico medico;
     private List<Medicamento> medicamentos;
+
+    public Receta(Paciente paciente, Medico medico, List<Medicamento> medicamentos) {
+        this.id = contadorIds++;
+        this.paciente = paciente;
+        this.medico = medico;
+        this.medicamentos = medicamentos;
+    }
 }
--- Practico06/src/main/java/Servicios/AtencionMedicoService.java
+++ Practico06/src/main/java/Servicios/AtencionMedicoService.java
@@ -1,4 +1,70 @@
 package Servicios;
 
+import Entidades.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class AtencionMedicoService {
+    private static AtencionMedicoService instancia;
+    private Contenedor contenedor;
+
+    public static void reiniciarInstancia() {
+        instancia = null;
+    }
+
+    private AtencionMedicoService(Contenedor contenedor) {
+        this.contenedor = contenedor;
+    }
+
+    public static synchronized AtencionMedicoService obtenerInstancia(Contenedor contenedor) {
+        if (instancia == null) {
+            instancia = new AtencionMedicoService(contenedor);
+        }
+        return instancia;
+    }
+
+    public void comenzarAtencion(Turno turno) {
+        turno.setEstado(true);
+        contenedor.getTurnoDAO().actualizar(turno);
+    }
+
+    public void crearReceta(Paciente paciente, Medico medico) {
+        double probabilidadReceta = 0.7;
+
+        Random random = new Random();
+        if (random.nextDouble() < probabilidadReceta) {
+            List<Medicamento> todosMedicamentos = contenedor.getMedicamentoDAO().listarTodos();
+
+            List<Medicamento> medicamentosReceta = generarMedicamentosAleatorios(todosMedicamentos);
+
+            Receta receta = new Receta(paciente, medico, medicamentosReceta);
+            contenedor.getRecetaDAO().crear(receta);
+            paciente.setTieneReceta(true);
+            contenedor.getPacienteDAO().actualizar(paciente);
+        } else {
+            paciente.setTieneReceta(false);
+            contenedor.getPacienteDAO().actualizar(paciente);
+        }
+    }
+
+    private List<Medicamento> generarMedicamentosAleatorios(List<Medicamento> todosMedicamentos) {
+        List<Medicamento> medicamentosReceta = new ArrayList<>();
+        Random random = new Random();
+        int cantidadMedicamentos = random.nextInt(4) + 1;
+
+        // Seleccionar medicamentos aleatorios de la lista
+        for (int i = 0; i < cantidadMedicamentos; i++) {
+            int indiceMedicamento = random.nextInt(todosMedicamentos.size());
+            medicamentosReceta.add(todosMedicamentos.get(indiceMedicamento));
+        }
+
+        return medicamentosReceta;
+    }
+
+    public void finalizarAtencion(Turno turno) {
+        turno.setEstado(false);
+        contenedor.getTurnoDAO().actualizar(turno);
+    }
 }
